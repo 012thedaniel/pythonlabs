15a85087faa5c2fff3ed45786b20b7eee67c6889
daniel <114145086+012thedaniel@users.noreply.github.com>
2022-10-14 01:19:06 +0300

    Update ex2.py

--- lab2/ex2.py
+++ lab2/ex2.py
@@ -7,46 +7,50 @@ class Rational:
             raise Exception(f'Numerator and denominator must be integer numbers')
         if not denominator:
             raise Exception(f'Denominator can\'t be equal to zero')
+        self.numerator = numerator
+        self.denominator = denominator
 
-        if numerator < 0 and denominator < 0:
-            numerator = -numerator
-            denominator = -denominator
-
-        greatest_common_divisor = math.gcd(numerator, denominator)
-        if not numerator or greatest_common_divisor == 1:
-            self.__numerator = numerator
-            self.__denominator = denominator
+    def rational(self):
+        if self.numerator < 0 and self.denominator < 0:
+            tmp_numerator = -self.numerator
+            tmp_denominator = -self.denominator
         else:
-            self.__numerator = int(numerator / greatest_common_divisor)
-            self.__denominator = int(denominator / greatest_common_divisor)
+            tmp_numerator = self.numerator
+            tmp_denominator = self.denominator
 
-    def print_rational(self):
-        print(f'Rational: {self.__numerator}/{self.__denominator}')
+        greatest_common_divisor = math.gcd(tmp_numerator, tmp_denominator)
 
-    def print_floating(self):
-        print(f'Floating-point number: {self.__numerator / self.__denominator}')
+        if not tmp_numerator or greatest_common_divisor == 1:
+            pass
+        else:
+            tmp_numerator = int(tmp_numerator/greatest_common_divisor)
+            tmp_denominator = int(tmp_denominator/greatest_common_divisor)
+        return f'{tmp_numerator}/{tmp_denominator}'
+
+    def floating(self):
+        return f'{self.numerator / self.denominator}'
 
 
 your_num = Rational()
-your_num.print_rational()
-your_num.print_floating()
+print(f'Rational: {your_num.rational()}')
+print(f'Floating-point number: {your_num.floating()}')
 
 your_num1 = Rational(4, 16)
-your_num1.print_rational()
-your_num1.print_floating()
+print(f'Rational: {your_num1.rational()}')
+print(f'Floating-point number: {your_num1.floating()}')
 
-your_num2 = Rational(1, 16)
-your_num2.print_rational()
-your_num2.print_floating()
+your_num2 = Rational(-1, -16)
+print(f'Rational: {your_num2.rational()}')
+print(f'Floating-point number: {your_num2.floating()}')
 
 your_num3 = Rational(0, 7)
-your_num3.print_rational()
-your_num3.print_floating()
+print(f'Rational: {your_num3.rational()}')
+print(f'Floating-point number: {your_num3.floating()}')
 
 your_num4 = Rational(9, 7)
-your_num4.print_rational()
-your_num4.print_floating()
+print(f'Rational: {your_num4.rational()}')
+print(f'Floating-point number: {your_num4.floating()}')
 
 your_num5 = Rational(3, 3)
-your_num5.print_rational()
-your_num5.print_floating()
+print(f'Rational: {your_num5.rational()}')
+print(f'Floating-point number: {your_num5.floating()}')
